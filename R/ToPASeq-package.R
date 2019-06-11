ccc1c4df1bc4d072a38cf245bd80d7d2dcf70147
unknown <ihnatova@iba.muni.cz>
2019-06-11 15:02:51 +0200

    clipper

--- R/ToPASeq-package.R
+++ R/ToPASeq-package.R
@@ -7,13 +7,11 @@
 #' @importFrom methods as is new slot slot<-
 #' @importFrom stats sd setNames model.matrix p.adjust t.test cov pchisq pf qchisq density median na.omit phyper pnorm qnorm cor rbinom rnorm
 #' @importFrom utils head
-#' @importFrom rrcov T2.test
 #' @importFrom graphics abline legend par points text
 #' @importFrom gRbase is.DAG getCliques
-#' @importFrom qpgraph qpIPF
 #' @importFrom graph subGraph validGraph degree
 #' @importFrom RBGL johnson.all.pairs.sp connectedComp floyd.warshall.all.pairs.sp
-#' @importFrom clipper pathQ cliqueMeanTest cliqueVarianceTest
 #' @importFrom limma voom lmFit eBayes topTable
+#' @importFrom corpcor cov.shrink
 #' @useDynLib ToPASeq
 NULL
